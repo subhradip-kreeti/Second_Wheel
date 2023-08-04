c027faa8c7fcadf6ad8cb1ea75b5c6e2f7177e97
subhradip-kreeti <subhradip.barik@kreeti.com>
2023-08-04 16:16:05 +0530

    change branch search parameter and modified few erb

---
+++ app/javascript/packs/readnotification.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+  var notificationCount = parseInt($("#notification-count").text());
+  if (notificationCount === 0) {
+    $("#readbtn").hide();
+  }
+});
+document.getElementById("readbtn").addEventListener("click", function (event) {
+  event.preventDefault();
+  var btnSelect = document.getElementById("readbtn");
+  var selectedBtn = btnSelect.value;
+
+  $.ajax({
+    url: "/markread",
+    method: "POST",
+    data: {
+      selected_btn: selectedBtn,
+      authenticity_token: $('meta[name="csrf-token"]').attr("content"),
+    },
+    success: function (response) {
+      var notificationCount = document.getElementById("notification-count");
+      if (notificationCount) {
+        notificationCount.innerText = "0";
+      }
+      var notificationList = document.getElementById("notification-list");
+      if (notificationList) {
+        notificationList.style.display = "none";
+      }
+    },
+    error: function (xhr, status, error) {
+      alert("Failed to mark as read");
+    },
+  });
+});
