31fd43d31823efd098340b08f6210e3ffda882c0
subhradip-kreeti <subhradip.barik@kreeti.com>
2023-08-16 13:37:22 +0530

    removed unnecessary comments from code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,5 @@
 
 # Home controller
 class HomeController < ApplicationController
-  # helper_method :current_user
-  # @current_user_email = session[:email]
   def welcome; end
-
-  # def current_user
-  #   @current_user ||= User.find_by(id: session[:user_id])
-  # end
 end
