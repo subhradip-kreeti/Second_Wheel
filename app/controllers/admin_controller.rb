4b1b0e7bdebebca841f7a1412a6531e208710121
subhradip-kreeti <subhradip.barik@kreeti.com>
2023-08-23 11:18:18 +0530

    fixed city add/remove flash

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -11,15 +11,20 @@ class AdminController < ApplicationController
 
   def add_city
     @city = City.create(name: params[:selected_city], state: params[:selected_state])
-    respond_to(&:js)
+
+    if @city.persisted?
+      handle_successful_city_addition
+    else
+      handle_failed_city_addition
+    end
   end
 
   def destroy_city
     city = City.find(params[:city_id])
     if city.destroy
-      flash[:danger] = 'City deleted successfully'
+      flash[:danger] = "City #{city.name} (#{city.state}) has been deleted successfully"
     else
-      flash[:error] = 'Failed to delete city'
+      flash[:error] = "Failed to delete #{city.name} (#{city.state}) city"
     end
     redirect_to show_city_path
   end
@@ -70,5 +75,18 @@ class AdminController < ApplicationController
       flash[:error] = 'Failed to delete Car Model'
     end
     redirect_to show_car_model_path
+  end
+
+  private
+
+  def handle_successful_city_addition
+    flash[:success] = "City #{@city.name} (#{@city.state}) has been added successfully"
+    respond_to(&:js)
+    redirect_to show_city_path
+  end
+
+  def handle_failed_city_addition
+    flash[:danger] = "Failed to add #{@city.name} (#{@city.state}) city"
+    redirect_to show_city_path
   end
 end
--- app/javascript/packs/show_city.js
+++ app/javascript/packs/show_city.js
@@ -1,77 +1,84 @@
-console.log("show city js loaded")
-
-document.querySelectorAll('.delete-city-btn').forEach(function(button) {
-  button.addEventListener('click', function(event) {
+document.querySelectorAll(".delete-city-btn").forEach(function (button) {
+  button.addEventListener("click", function (event) {
     event.preventDefault();
-    var cityId = this.getAttribute('data-city-id');
-    var cityName = this.getAttribute('data-city-name');
-    var stateName = this.getAttribute('data-state-name');
-    if (confirm(`Are you sure you want to delete ${cityName} (${stateName}) city?`))
-    $.ajax({
-      url: '/delete_city/' + cityId,
-      method: 'DELETE',
-      data: {
-        city_id: cityId,
-        authenticity_token: $('meta[name="csrf-token"]').attr('content')
-      },
-      success: function(response) {
-        event.target.closest('li').remove();
-        // alert('City deleted successfully');
-      },
-      error: function(xhr, status, error) {
-        // alert('Failed to delete city');
-      }
-    });
-
+    var cityId = this.getAttribute("data-city-id");
+    var cityName = this.getAttribute("data-city-name");
+    var stateName = this.getAttribute("data-state-name");
+    if (
+      confirm(
+        `Are you sure you want to delete ${cityName} (${stateName}) city?`
+      )
+    )
+      $.ajax({
+        url: "/delete_city/" + cityId,
+        method: "DELETE",
+        data: {
+          city_id: cityId,
+          authenticity_token: $('meta[name="csrf-token"]').attr("content"),
+        },
+        success: function (response) {
+          console.log("reached")
+          event.target.closest("li").remove();
+        },
+        error: function (xhr, status, error) {},
+      });
   });
 });
 
-document.getElementById('add-city-btn').addEventListener('click', function(event) {
-  // event.preventDefault();
-  var citySelect = document.getElementById('city-input');
-  var stateSelect = document.getElementById('state-input');
-  var selectedCity = citySelect.value;
-  var selectedState = stateSelect.value;
-  console.log(selectedCity)
-  console.log(selectedState)
-  if(selectedCity.trim() == '' || selectedState.trim() == '')
-  {
-    alert("Enter values in Both fields");
-    return;
-  }
-  if (isCityStatePairAlreadyPresent(selectedCity, selectedState)) {
-    alert("City and State Pair already exists.");
-    document.getElementById('city-input').value = '';
-    document.getElementById('state-input').value = '';
-    return;
-  }
-
-  $.ajax({
-    url: '/add_city',
-    method: 'POST',
-    data: {
-      selected_city: selectedCity,
-      selected_state: selectedState,
-      authenticity_token: $('meta[name="csrf-token"]').attr('content')
-    },
-    success: function(response) {
-      alert("City added successfully");
-    },
-    error: function(xhr, status, error) {
-      alert("Failed to add city");
+document
+  .getElementById("add-city-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    var citySelect = document.getElementById("city-input");
+    var stateSelect = document.getElementById("state-input");
+    var selectedCity = citySelect.value;
+    var selectedState = stateSelect.value;
+    console.log(selectedCity);
+    console.log(selectedState);
+    if (selectedCity.trim() == "" || selectedState.trim() == "") {
+      alert("Enter values in Both fields");
+      return;
     }
+    if (isCityStatePairAlreadyPresent(selectedCity, selectedState)) {
+      alert("City and State Pair already exists.");
+      document.getElementById("city-input").value = "";
+      document.getElementById("state-input").value = "";
+      return;
+    }
+    if (
+      confirm(
+        `Are you sure you want to add ${selectedCity} (${selectedState}) city?`
+      )
+    )
+      $.ajax({
+        url: "/add_city",
+        method: "POST",
+        data: {
+          selected_city: selectedCity,
+          selected_state: selectedState,
+          authenticity_token: $('meta[name="csrf-token"]').attr("content"),
+        },
+        success: function (response) {},
+        error: function (xhr, status, error) {},
+      });
   });
-});
 
 function isCityStatePairAlreadyPresent(city, state) {
-  var cityList = document.getElementById('city-list');
-  var listItems = cityList.getElementsByTagName('li');
+  var cityList = document.getElementById("city-list");
+  var listItems = cityList.getElementsByTagName("li");
   for (var i = 0; i < listItems.length; i++) {
     var listItemText = listItems[i].innerText.toLowerCase();
-    var listItemCity = listItemText.split('(')[0].trim().toLowerCase();
-    var listItemState = listItemText.split('(')[1].replace(')', '').trim().toLowerCase();
+    var listItemCity = listItemText.split("(")[0].trim().toLowerCase();
+    var listItemState = listItemText
+      .split("(")[1]
+      .replace(")", "")
+      .trim()
+      .toLowerCase();
 
-    if (listItemCity === city.toLowerCase() && listItemState === state.toLowerCase()) {
+    if (
+      listItemCity === city.toLowerCase() &&
+      listItemState === state.toLowerCase()
+    ) {
       return true;
     }
   }
